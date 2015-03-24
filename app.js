ebfe8663ad888a96ecb65c660825b6e7207c37f0
eedawa <eedawa@gmail.com>
2015-03-24 12:08:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
 
 // hello world route
-app.post('/hello', bigbang);
+app.post('/hello', hellobot);
 
 // error handler
 app.use(function (err, req, res, next) {
