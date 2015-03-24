f361afc2787f247f89d73363a25c16e840171e07
eedawa <eedawa@gmail.com>
2015-03-24 11:52:48 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ module.exports = function (req, res, next) {
   var botPayload = {
     text : 'Wow, fantastic baby!'
   };
-  if(userName !== "bigbangslackbot"){
+  if (req.body.user_name !== "bigbangslackbot"){
     return res.status(200).json(botPayload);
   };
 }
