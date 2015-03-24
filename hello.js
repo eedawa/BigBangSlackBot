d36c13f225dc94721a13e759174c4add0c329dd2
eedawa <eedawa@gmail.com>
2015-03-24 11:41:20 +0900

    Update hello.js
    Added line of code to avoid infinite loop.

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ module.exports = function (req, res, next) {
   var botPayload = {
     text : 'Wow, fantastic baby!'
   };
-
+  if(userName !== "bigbangslackbot")
   return res.status(200).json(botPayload);
 }
